76a3e57eb12babada49bcccd3d9a079358d00ff5
xinri <h.lay@gop.link>
2019-02-11 00:49:20 +0100

    finish for back case and all direction

--- src/main/java/Ant.java
+++ src/main/java/Ant.java
@@ -1,18 +1,28 @@
 public class Ant {
 
+  private Direction direction;
+
   public void setPosition(int x, int y) {
 
   }
 
   public void setDirection(Direction direction) {
-
+    this.direction = direction;
   }
 
   public void move(Board board) {
-
+    if (direction == Direction.NORTH) {
+      direction = Direction.WEST;
+    } else if (direction == Direction.WEST) {
+      direction = Direction.SOUTH;
+    } else if (direction == Direction.SOUTH) {
+      direction = Direction.EAST;
+    } else {
+      direction = Direction.NORTH;
+    }
   }
 
   public Direction getDirection() {
-    return Direction.NORTH;
+    return direction;
   }
 }
--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -21,4 +21,54 @@ public class BoardShould {
     assertThat(ant.getDirection() == Direction.NORTH).isTrue();
   }
 
+  @Test
+  public void stay_and_face_west_when_ant_is_in_a_black_case_and_face_north() {
+    // given
+    Board board = new Board();
+    board.setBoard(new String[]{"b"});
+    Ant ant = new Ant();
+    ant.setPosition(0, 0);
+    ant.setDirection(Direction.NORTH);
+
+    // when
+    ant.move(board);
+
+    // then
+    assertThat(board.getCase(0, 0)).isEqualTo("n");
+    assertThat(ant.getDirection() == Direction.WEST).isTrue();
+  }
+
+  @Test
+  public void stay_and_face_south_when_ant_is_in_a_black_case_and_face_west() {
+    // given
+    Board board = new Board();
+    board.setBoard(new String[]{"b"});
+    Ant ant = new Ant();
+    ant.setPosition(0, 0);
+    ant.setDirection(Direction.WEST);
+
+    // when
+    ant.move(board);
+
+    // then
+    assertThat(board.getCase(0, 0)).isEqualTo("n");
+    assertThat(ant.getDirection() == Direction.SOUTH).isTrue();
+  }
+
+  @Test
+  public void stay_and_face_east_when_ant_is_in_a_black_case_and_face_south() {
+    // given
+    Board board = new Board();
+    board.setBoard(new String[]{"b"});
+    Ant ant = new Ant();
+    ant.setPosition(0, 0);
+    ant.setDirection(Direction.SOUTH);
+
+    // when
+    ant.move(board);
+
+    // then
+    assertThat(board.getCase(0, 0)).isEqualTo("n");
+    assertThat(ant.getDirection() == Direction.EAST).isTrue();
+  }
 }
